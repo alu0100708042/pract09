3d02b0fbe810e63acbfbb4f39c1672abf0cac523
Ricardo Oliva <alu0100708042@ull.edu.es>
2013-11-20 23:24:23 +0000

    Añandiendo métodos min y max

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -1,5 +1,6 @@
 require './matrix.rb'
 require './sparse_vector.rb'
+require './dense_matrix.rb'
 
 # Clase para la representacion de matrices dispersas.
 class SparseMatrix < Matrix
@@ -8,15 +9,19 @@ class SparseMatrix < Matrix
 
   def initialize(h = {})
     @m_Matrix = Hash.new({})
-    for k in h.keys do 
+     row= 0
+    for k in h.keys do
+      row = k  if (row < k.to_i == true)   
       if h[k].is_a? SparseVector
         @m_Matrix[k] = h[k]
       else 
         @m_Matrix[k] = SparseVector.new(h[k])
       end
     end
+    super(row,row)
   end
-
+ 
+  
   def [](i)
     @m_Matrix[i]
   end
@@ -33,7 +38,7 @@ class SparseMatrix < Matrix
 	
 	sumita={}
 		
-	#a.m_Matrix[1000].vector.keys[0]
+	
 	
 	for r in @m_Matrix.keys do 
 		sum = {}
@@ -43,20 +48,53 @@ class SparseMatrix < Matrix
 				sum[j] = @m_Matrix[r].vector[j]+matrizb[r].vector[j]	
 				
 				sumita[r] = {j=>sum[j]}
-				#puts "#{r} => #{sumita[r]}"
 			end
 			
 		else
 			
 			sum[r]=@m_Matrix[r]
 		end
-		#for j in vector.keys do
-		#	@m_Matrix[r][j]+matrizb[r][j]	
-		#end
+	
 	
 	end
 
    SparseMatrix.new(sumita)
   end
+  
+   # Se define un metodo para hallar el máximo que retornará un número
 
+   def max(other)
+	max = 0
+	for r in @m_Matrix.keys do
+		for j in @m_Matrix[r].vector.keys do 
+			max = @m_Matrix[r].vector[j] if (max < @m_Matrix[r].vector[j] == true)		
+		end
+	end
+	for r in other.m_Matrix.keys do
+		for j in other.m_Matrix[r].vector.keys do 
+			max = other.m_Matrix[r].vector[j] if (max < other.m_Matrix[r].vector[j] == true)	
+		end
+	
+	end
+	max
+   end
+
+     # Se define un metodo para hallar el máximo que retornará un número
+
+   def min(other)
+	min = 1000000
+	for r in @m_Matrix.keys do
+		for j in @m_Matrix[r].vector.keys do 
+			min = @m_Matrix[r].vector[j] if (min > @m_Matrix[r].vector[j] == true)	
+		end
+	
+	end
+	for r in other.m_Matrix.keys do
+		for j in other.m_Matrix[r].vector.keys do 
+			min = other.m_Matrix[r].vector[j] if (min > other.m_Matrix[r].vector[j] == true)	
+		end
+	
+	end
+	min
+   end
 end
