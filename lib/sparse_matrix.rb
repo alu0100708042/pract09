b42faa497bc6cbd6b1e47780b8c27d9f310e56e9
Oliver Martínez Novo <alu0100704129@ull.edu.es>
2013-11-21 23:36:52 +0000

    Completando pruebas rspec

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -5,122 +5,125 @@ require './dense_matrix.rb'
 # Clase para la representacion de matrices dispersas.
 class SparseMatrix < Matrix
 
-  attr_reader :m_Matrix
+	attr_reader :m_Matrix
 
-  def initialize(h = {})
-    @m_Matrix = Hash.new({})
-     row, col = 0, 0
-     arrayCol=[]
-     j = 0
-    for k in h.keys do
-      row = k  if (row < k.to_i == true)   
-      if h[k].is_a? SparseVector
-        @m_Matrix[k] = h[k]
-      else 
-        @m_Matrix[k] = SparseVector.new(h[k])
-      end
-
-    end
-
-    for r in @m_Matrix.keys do
-	j= 0
-	while j <  @m_Matrix[r].vector.keys.size do 
-	     col= @m_Matrix[r].vector.keys[j] if (col < @m_Matrix[r].vector.keys[j].to_i == true)
-	     j += 1		
-	end
-    end
-    super(row,col)
-  end
- 
-  
-  def [](i)
-    @m_Matrix[i]
-  end
-
-  def col(j)
-    c = {}
-    for r in @m_Matrix.keys do   
-	c[r] = @m_Matrix[r].vector[j] if @m_Matrix[r].vector.keys.include? j
-    end
-    SparseVector.new c
-  end
-  
-  def +(matrizb)
-	
-	sumita={}
-		
-	
-	
-	for r in @m_Matrix.keys do 
-		sum = {}
-		if matrizb.m_Matrix.keys.include? r
-			for j in @m_Matrix[r].vector.keys do 
-				
-				sum[j] = @m_Matrix[r].vector[j]+matrizb[r].vector[j]	
-				
-				sumita[r] = {j=>sum[j]}
+	def initialize(h = {})
+		@m_Matrix = Hash.new({})
+		row, col = 0, 0
+		arrayCol=[]
+		j = 0
+		for k in h.keys do
+			row = k  if (row < k.to_i == true)   
+			if h[k].is_a? SparseVector
+				@m_Matrix[k] = h[k]
+			else 
+				@m_Matrix[k] = SparseVector.new(h[k])
 			end
-			
-		else
-			
-			sum[r]=@m_Matrix[r]
 		end
-	
-	
-	end
-   SparseMatrix.new(sumita)
-  end
-  
-   # Se define un metodo para hallar el máximo que retornará un número
 
-   def max(other)
-	max = -1000000
-	for r in @m_Matrix.keys do
-		for j in @m_Matrix[r].vector.keys do 
-			max = @m_Matrix[r].vector[j] if (max < @m_Matrix[r].vector[j] == true)		
-		end
-	end
-	for r in other.m_Matrix.keys do
-		for j in other.m_Matrix[r].vector.keys do 
-			max = other.m_Matrix[r].vector[j] if (max < other.m_Matrix[r].vector[j] == true)	
-		end
-	
-	end
-	max
-   end
-
-     # Se define un metodo para hallar el máximo que retornará un número
-
-   def min(other)
-	min = 1000000
-	for r in @m_Matrix.keys do
-		for j in @m_Matrix[r].vector.keys do 
-			min = @m_Matrix[r].vector[j] if (min > @m_Matrix[r].vector[j] == true)	
-		end
-	
-	end
-	for r in other.m_Matrix.keys do
-		for j in other.m_Matrix[r].vector.keys do 
-			min = other.m_Matrix[r].vector[j] if (min > other.m_Matrix[r].vector[j] == true)	
-		end
-	
-	end
-	min
-   end
-   def to_s
-	 str = "["	
 		for r in @m_Matrix.keys do
-			
-			for j in @m_Matrix[r].vector.keys do
-				
-			str <<	"[#{r}] [#{@m_Matrix[r].vector.to_s}]"	
-			  #puts "[#{r}] [#{j}]"	
+			j= 0
+			while j <  @m_Matrix[r].vector.keys.size do 
+				 col= @m_Matrix[r].vector.keys[j] if (col < @m_Matrix[r].vector.keys[j].to_i == true)
+				 j += 1		
 			end
-    	
+		end
+		super(row,col)
+	end
+
+	# Metodo para la indexacion
+	def [](i)
+		@m_Matrix[i]
+	end
+
+	# Metodo que retorna un SparseVector con los valores de una columna.
+	def col(j)
+		c = {}
+		for r in @m_Matrix.keys do   
+			c[r] = @m_Matrix[r].vector[j] if @m_Matrix[r].vector.keys.include? j
+		end
+		SparseVector.new c
+	end
+
+	# Metodo para la operacion aritmetica de la suma.
+	def +(matrizb)
+		sumita={}
+		for r in @m_Matrix.keys do 
+			sum = {}
+			if matrizb.m_Matrix.keys.include? r
+				for j in @m_Matrix[r].vector.keys do 
+					sum[j] = @m_Matrix[r].vector[j]+matrizb[r].vector[j]	
+					sumita[r] = {j=>sum[j]}
+				end
+			else
+				sum[r]=@m_Matrix[r]
+			end
+		end
+		SparseMatrix.new(sumita)
+	end
+
+	# Metodo para la operacion aritmetica de la resta.
+	def -(matrizb)
+		restita={}
+		for r in @m_Matrix.keys do 
+			res = {}
+			if matrizb.m_Matrix.keys.include? r
+				for j in @m_Matrix[r].vector.keys do 
+					res[j] = @m_Matrix[r].vector[j]-matrizb[r].vector[j]	
+					restita[r] = {j=>res[j]}
+				end
+			else
+				res[r]=@m_Matrix[r]
+			end
+		end
+		SparseMatrix.new(restita)
+	end
+		
+	# Se define un metodo para hallar el máximo que retornará un número
+	def max(other)
+		max = -1000000
+		for r in @m_Matrix.keys do
+			for j in @m_Matrix[r].vector.keys do 
+				max = @m_Matrix[r].vector[j] if (max < @m_Matrix[r].vector[j] == true)		
+			end
+		end
+		for r in other.m_Matrix.keys do
+			for j in other.m_Matrix[r].vector.keys do 
+				max = other.m_Matrix[r].vector[j] if (max < other.m_Matrix[r].vector[j] == true)	
+			end
+		end
+		max
+	end
+
+    # Se define un metodo para hallar el máximo que retornará un número
+	def min(other)
+		min = 1000000
+		for r in @m_Matrix.keys do
+			for j in @m_Matrix[r].vector.keys do 
+				min = @m_Matrix[r].vector[j] if (min > @m_Matrix[r].vector[j] == true)	
+			end
+		end
+		for r in other.m_Matrix.keys do
+			for j in other.m_Matrix[r].vector.keys do 
+				min = other.m_Matrix[r].vector[j] if (min > other.m_Matrix[r].vector[j] == true)	
+			end
+		end
+		min
+	end
+   
+	def to_s
+		str = "["	
+		for r in @m_Matrix.keys do
+			for j in @m_Matrix[r].vector.keys do	
+			str <<	"[#{r}] [#{@m_Matrix[r].vector.to_s}]"	
+			#puts "[#{r}] [#{j}]"	
+			end
 		end	
 		str << "]"	
 	end
-    def coerce(other)
-	return  [self,other]
-    end 	
+    
+	def coerce(other)
+		return  [self,other]
+    end
+ 	
 end
